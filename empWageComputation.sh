6a99c4f516501d3ad311047ab1f13a356e762010
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-01-18 14:02:18 +0530

    Add: Calculate Daily Employee Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,18 @@
 echo "Welcome to origin masterEmployee Wage Computation Program"
+
+ispresent=0;
+randomCheck=$(( RANDOM%2 ));
+if [ $ispresent -eq $randomCheck ];
+then
+        empRateperh=20;
+        emph=8;
+        salary=$(( $empRateperh*$emph ));
+        echo $salary;
+else
+    salary=0;
+    echo $salary
+fi
+
+
+
+
