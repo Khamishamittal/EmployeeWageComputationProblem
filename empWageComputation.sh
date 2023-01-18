40fc536fa6738f37443e1996d98c048457a6344a
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-01-18 13:27:21 +0530

    Add: Check Employee is present or absent

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,13 @@
 echo "Welcome to origin masterEmployee Wage Computation Program"
+
+ispresent=0;
+randomCheck=$(( RANDOM%2 ));
+
+if [ $ispresent -eq $randomCheck ]
+then
+        echo "employee is present";
+else
+        echo "employee is absent"
+fi
+
+
